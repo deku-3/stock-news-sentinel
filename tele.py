18f3a0afe170f0051bad2f4a1aaef8156431918c
aditya11 <aditya.kumar@accordion.com>
2025-08-01 00:49:16 +0530

    Use environment variables for Telegram bot token and chat ID

--- tele.py
+++ tele.py
@@ -1,9 +1,12 @@
+import os
 import requests
-
+from dotenv import load_dotenv
+load_dotenv()
 
 def send_telegram_message(message):
-    TELEGRAM_BOT_TOKEN = "7764932403:AAHMiZSI33jgTglCn6k80da804sjdPDLBSs" 
-    TELEGRAM_CHAT_ID = 1065095420  
+
+    TELEGRAM_BOT_TOKEN=os.getenv("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_CHAT_ID=os.getenv(TELEGRAM_CHAT_ID)
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
